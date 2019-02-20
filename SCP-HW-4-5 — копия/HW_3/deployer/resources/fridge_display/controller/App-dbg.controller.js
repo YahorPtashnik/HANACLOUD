c99e95c31b2ba8f97dd2d94b6ee159e98b897b64
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2019-02-20 10:39:58 +0300

    Edit\Save buttons are done for each table row

--- SCP-HW-4-5 — копия/HW_3/deployer/resources/fridge_display/controller/App-dbg.controller.js
+++ SCP-HW-4-5 — копия/HW_3/deployer/resources/fridge_display/controller/App-dbg.controller.js
@@ -2,21 +2,39 @@ sap.ui.define([
     "fridge_display/controller/BaseController"
 ], function (BaseController) {
     "use strict";
+    this.editOn = function(items, index) {
+        items[index].getCells()[5].setEnabled(false);
+        items[index].getCells()[6].setEnabled(true);
+    },
+    this.editOff = function(items, index) {
+        for (var j = 0; j < 5; j++) {
+            items[index].getCells()[j].setEditable(false);
+        }
+        items[index].getCells()[5].setEnabled(true);
+        items[index].getCells()[6].setEnabled(false);
+    }
     return BaseController.extend("fridge_display.controller.App", {
+        onInit: function () {
+
+        },
         listFactory: function (sId) {
             var oUIControl;
-
             oUIControl = this.byId("item").clone(sId);
             console.log(sId);
             return oUIControl;
         },
         goToDetails: function (oEvent) {
-            var oSelectedItem = oEvent.getSource();
-            var context = encodeURIComponent(oSelectedItem.getBindingContext('Fridges').getPath());
-            console.log(context);
-            this.getRouter().navTo("details", {
-                id: context
-            });
+            var oTable = this.getView().byId('details');
+            var selItem = oTable.getSelectedItem();
+            var aItems = oTable.getItems();
+            var index = oTable.indexOfItem(selItem);
+            console.log(index);
+            for (var j = 0; j < 3; j++) {
+                aItems[index].getCells()[j].setEditable(aItems[index].getSelected());
+                console.log(aItems[index].getSelected());
+            }
+            editOn(aItems, index);
+            
         },
         createFridge: function () {
             var Name = this.getView().byId("brandNameInput").getValue();
@@ -34,6 +52,33 @@ sap.ui.define([
                 "processData": false,
                 "data": "{\"bname\": \"" + Name + "\", \"cap\": \"" + Cap + "\"}"
             };
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+        },
+        updateFridge: function () {
+            var oTable = this.getView().byId('details');
+            var selItem = oTable.getSelectedItem();
+            var aItems = oTable.getItems();
+            var index = oTable.indexOfItem(selItem);
+            var bname = aItems[index].getCells()[1].getValue();
+            var cap = aItems[index].getCells()[2].getValue();
+            var frid = selItem.getBindingContext("fridges").getObject().frid;
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsodata/HW_3.xsodata/Fridges('" + frid + "')",
+                "method": "PUT",
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "processData": false,
+                "data": "{\"bname\": \"" + bname + "\", \"cap\": \"" + cap + "\", \"ts_update\":null, \"ts_create\":null}"
+            };
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+            editOff(aItems, index);
         }
     });
 });
--- SCP-HW-4-5 — копия/HW_3/web/fridge_display/webapp/controller/App.controller.js
+++ SCP-HW-4-5 — копия/HW_3/web/fridge_display/webapp/controller/App.controller.js
@@ -2,11 +2,20 @@ sap.ui.define([
     "fridge_display/controller/BaseController"
 ], function (BaseController) {
     "use strict";
+    this.editOn = function(items, index) {
+        items[index].getCells()[5].setEnabled(false);
+        items[index].getCells()[6].setEnabled(true);
+    },
+    this.editOff = function(items, index) {
+        for (var j = 0; j < 5; j++) {
+            items[index].getCells()[j].setEditable(false);
+        }
+        items[index].getCells()[5].setEnabled(true);
+        items[index].getCells()[6].setEnabled(false);
+    }
     return BaseController.extend("fridge_display.controller.App", {
-        onInit: function() {
-            this.getView().byId("idIconTabBarFiori2").setVisible(false);
-            this.getView().byId("updateSomePanel").setVisible(false);
-            this.getView().byId("createSomePanel").setVisible(false);
+        onInit: function () {
+
         },
         listFactory: function (sId) {
             var oUIControl;
@@ -15,14 +24,17 @@ sap.ui.define([
             return oUIControl;
         },
         goToDetails: function (oEvent) {
-            var oTable = this.getView().byId("details");
-            var fridge = oTable.getSelectedItem().getBindingContext("fridges").getObject();
-            this.getView().byId("fridInput_U").setValue(fridge.frid);
-            this.getView().byId("brandNameInput_U").setValue(fridge.bname);
-            this.getView().byId("capNameInput_U").setValue(fridge.cap);
-            this.getView().byId("idIconTabBarFiori2").setVisible(true);
-            this.getView().byId("createSomePanel").setVisible(false);
-            this.getView().byId("updateSomePanel").setVisible(true);
+            var oTable = this.getView().byId('details');
+            var selItem = oTable.getSelectedItem();
+            var aItems = oTable.getItems();
+            var index = oTable.indexOfItem(selItem);
+            console.log(index);
+            for (var j = 0; j < 3; j++) {
+                aItems[index].getCells()[j].setEditable(aItems[index].getSelected());
+                console.log(aItems[index].getSelected());
+            }
+            editOn(aItems, index);
+            
         },
         createFridge: function () {
             var Name = this.getView().byId("brandNameInput").getValue();
@@ -45,37 +57,28 @@ sap.ui.define([
             });
         },
         updateFridge: function () {
-            var oTable = this.getView().byId("details");
-            var oSelectedItem = oTable.getSelectedItem();
-            var index = oTable.indexOfItem(oSelectedItem);
-            console.log(index);
-            var bname = this.getView().byId("brandNameInput_U").getValue();
-            var cap = this.getView().byId("capNameInput_U").getValue();
-            if (!index) {
-                sap.m.MessageToast.show("Fridge is not selected!");
-            } else if (!bname || !cap) {
-                sap.m.MessageToast.show("Insert all required fields with data!");
-            } else {
-                var frid = oSelectedItem.getBindingContext("fridges").getObject().frid;
-                var settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsodata/HW_3.xsodata/Fridges('" + frid + "')",
-                    "method": "PUT",
-                    "headers": {
-                        "content-type": "application/json"
-                    },
-                    "processData": false,
-                    "data": "{\"bname\": \"" + bname + "\", \"cap\": \"" + cap + "\", \"ts_update\":null, \"ts_create\":null}"
-                };
-                console.log(bname);
-                console.log(cap);
-                console.log(settings.url);
-                $.ajax(settings).done(function (response) {
-                    console.log(response);
-                });
-                window.location.reload();
-            }
+            var oTable = this.getView().byId('details');
+            var selItem = oTable.getSelectedItem();
+            var aItems = oTable.getItems();
+            var index = oTable.indexOfItem(selItem);
+            var bname = aItems[index].getCells()[1].getValue();
+            var cap = aItems[index].getCells()[2].getValue();
+            var frid = selItem.getBindingContext("fridges").getObject().frid;
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsodata/HW_3.xsodata/Fridges('" + frid + "')",
+                "method": "PUT",
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "processData": false,
+                "data": "{\"bname\": \"" + bname + "\", \"cap\": \"" + cap + "\", \"ts_update\":null, \"ts_create\":null}"
+            };
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+            editOff(aItems, index);
         }
     });
 });
