665bc94509b4ada5ea77544232b2e5707c8b3e47
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2019-02-24 17:55:32 +0300

    Final task completed

--- SCP-HW-4-5 — копия/HW_3/web/fridge_display/webapp/controller/App.controller.js
+++ SCP-HW-4-5 — копия/HW_3/web/fridge_display/webapp/controller/App.controller.js
@@ -1,169 +1,217 @@
 sap.ui.define([
     "fridge_display/controller/BaseController",
-    'sap/m/MessageBox',
     'sap/m/MessageToast',
     "sap/ui/core/Fragment"
-], function (BaseController, MessageBox, MessageToast, Fragment) {
+], function (BaseController, MessageToast, Fragment) {
     "use strict";
-    this.editOn = function (eItems, eIndex) {
-        eItems[eIndex].getCells()[5].setEnabled(false);
-        eItems[eIndex].getCells()[6].setEnabled(true);
-    };
-
-    this.editOff = function (wItems, wIndex) {
-        for (let j = 0; j < 5; j++) {
-            wItems[wIndex].getCells()[j].setEditable(false);
-        }
-        wItems[wIndex].getCells()[5].setEnabled(true);
-        wItems[wIndex].getCells()[6].setEnabled(false);
-    };
-
-    this.disableRowFields = function (aItems) {
-        for (let i = 0; i < aItems.length; i++) {
-            for (let j = 5; j < 8; j++) {
-                aItems[i].getCells()[j].setEnabled(false);
-                aItems[i].getCells()[1].setEditable(false);
-                aItems[i].getCells()[2].setEditable(false);
-            }
-        }
-    };
-
-    this.getTableSelectedComponent = function (table) {
-        let oResult = {
-            selItem: table.getSelectedItem(),
-            aItems: table.getItems(),
-            index: table.indexOfItem(table.getSelectedItem())
-        };
-        return oResult;
-    }
-
-    return BaseController.extend("fridge_display.controller.App", {
-        onInit: function () {
-
-        },
-        goToDetails: function () {
-            let oTable = this.getView().byId('details');
-            let obj = getTableSelectedComponent(oTable);
-            for (let j = 1; j < 3; j++) {
-                obj.aItems[obj.index].getCells()[j].setEditable(obj.aItems[obj.index].getSelected());
-            }
-            editOn(obj.aItems, obj.index, obj.oTable);
-        },
-        showCreateDialog: function () {
-            let oView = this.getView();
-            if (!this.byId("createDialog")) {
-                Fragment.load({
-                    id: oView.getId(),
-                    name: "fridge_display.view.Create",
-                    controller: this
-                }).then(function (oDialog) {
-                    oView.addDependent(oDialog);
-                    oDialog.open();
-                });
-            } else {
-                this.byId("createDialog").open();
-            }
-        },
-        closeDialog: function () {
-            this.getView().byId("createDialog").close();
-        },
-        createFridge: function () {
-            let Name = this.byId("newBrandNameInput").getValue();
-            let Cap = this.byId("newCapInput").getValue();
-            if (!Name || !Cap) {
-                let dialog = new sap.m.Dialog({
-                    title: 'Error',
-                    type: 'Message',
-                    state: 'Error',
-                    content: new sap.m.Text({
-                        text: 'Enter all necessarry field'
-                    }),
-                    beginButton: new sap.m.Button({
-                        text: 'OK',
-                        press: function () {
-                            dialog.close();
-                        }
-                    }),
-                    afterClose: function () {
-                        dialog.destroy();
+    this.createUnvalidDataExceptionDialog = function () {
+            let dialog = new sap.m.Dialog({
+                title: 'Error',
+                type: 'Message',
+                state: 'Error',
+                content: new sap.m.Text({
+                    text: "Enter valid data"
+                }),
+                beginButton: new sap.m.Button({
+                    text: 'OK',
+                    press: function () {
+                        dialog.close();
                     }
-                });
-                dialog.open();
-            } else {
-                let oTable = this.getView().byId('details');
-                let settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsodata/HW_3.xsodata/Fridges",
-                    "method": "POST",
-                    "headers": {
-                        "content-type": "application/json"
-                    },
-                    "processData": false,
-                    "data": "{\"bname\": \"" + Name + "\", \"cap\": \"" + Cap + "\"}"
-                };
-                $.ajax(settings).done(function (response) {
-                    oTable.getModel("fridges").refresh(true);
-                });
-                this.byId("createDialog").close();
-            }
-        },
-        updateFridge: function () {
-            let oTable = this.getView().byId('details');
-            let obj = getTableSelectedComponent(oTable);
-            let bname = obj.aItems[obj.index].getCells()[1].getValue();
-            let cap = obj.aItems[obj.index].getCells()[2].getValue();
-            let frid = obj.selItem.getBindingContext("fridges").getObject().frid;
-            let settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsodata/HW_3.xsodata/Fridges('" + frid + "')",
-                "method": "PUT",
-                "headers": {
-                    "content-type": "application/json"
-                },
-                "processData": false,
-                "data": "{\"bname\": \"" + bname + "\", \"cap\": \"" + cap + "\", \"ts_update\":null, \"ts_create\":null}"
-            };
-            $.ajax(settings).done(function (response) {
-                obj.selItem.getBindingContext("fridges").getModel().refresh(true);
+                }),
+                afterClose: function () {
+                    dialog.destroy();
+                }
             });
-            editOff(obj.aItems, obj.index, oTable);
-            let bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
-            MessageBox.success(
-                "Fridge number " + frid + " has been changed!", {
-                    styleClass: bCompact ? "sapUiSizeCompact" : ""
-                }
-            );
+            dialog.open();
         },
-        selectFridge: function () {
-            let oTable = this.getView().byId('details');
-            let obj = getTableSelectedComponent(oTable);
-            disableRowFields(obj.aItems);
-            if (!obj.aItems[obj.index].getCells()[6].getEnabled()) {
-                for (let i = 0; i < obj.aItems.length; i++) {
-                    obj.aItems[i].getCells()[5].setEnabled(obj.aItems[i].getSelected());
-                    obj.aItems[i].getCells()[7].setEnabled(obj.aItems[i].getSelected());
+        this.createSuccessOnUpdateDialog = function () {
+            let dialog = new sap.m.Dialog({
+                title: 'Success',
+                type: 'Message',
+                state: 'Success',
+                content: new sap.m.Text({
+                    text: "Data is successfully changed"
+                }),
+                beginButton: new sap.m.Button({
+                    text: 'OK',
+                    press: function () {
+                        dialog.close();
+                    }
+                }),
+                afterClose: function () {
+                    dialog.destroy();
+                }
+            });
+            dialog.open();
+        },
+        this.createSuccessOnCreateDialog = function () {
+            let dialog = new sap.m.Dialog({
+                title: 'Success',
+                type: 'Message',
+                state: 'Success',
+                content: new sap.m.Text({
+                    text: "Data is successfully added"
+                }),
+                beginButton: new sap.m.Button({
+                    text: 'OK',
+                    press: function () {
+                        dialog.close();
+                    }
+                }),
+                afterClose: function () {
+                    dialog.destroy();
+                }
+            });
+            dialog.open();
+        },
+        ///ERROR HANDLING & VALIDATION
+        this.checkModelObject = function (model) {
+            console.log(model);
+
+            if (!model.bname || !model.cap || model.bname.length > 100 || model.cap.length > 6) {
+                createUnvalidDataExceptionDialog();
+            } else {
+                return true;
+            }
+        },
+        ///ROW PROCESSING METHODS
+        this.getTableRowIndex = function (oEvent, table, modelName, idPropName) {
+            let frid = oEvent.getSource().getBindingContext(modelName).getProperty(idPropName);
+            let tItems = table.getItems();
+            for (let i = 0; i < tItems.length; i++) {
+                if (frid === tItems[i].getCells()[0].getValue()) {
+                    return i;
                 }
             }
         },
-        deleteFridge: function () {
-            let oTable = this.getView().byId('details');
-            let obj = getTableSelectedComponent(oTable);
-            let frid = obj.selItem.getBindingContext("fridges").getObject().frid;
-            let settings = {
+        this.editMode = function (items, index, config) {
+            if (items[index].getCells()[config.editBtnPosition].getEnabled()) {
+                items[index].getCells()[config.editBtnPosition].setEnabled(false);
+                items[index].getCells()[config.saveBtnPosition].setEnabled(true);
+                items[index].getCells()[config.deleteBtnPosition].setEnabled(true);
+                items[index].getCells()[config.brandNameInputPosition].setEditable(true);
+                items[index].getCells()[config.capacityInputPosition].setEditable(true);
+            } else {
+                items[index].getCells()[config.editBtnPosition].setEnabled(true);
+                items[index].getCells()[config.saveBtnPosition].setEnabled(false);
+                items[index].getCells()[config.deleteBtnPosition].setEnabled(false);
+                items[index].getCells()[config.brandNameInputPosition].setEditable(false);
+                items[index].getCells()[config.capacityInputPosition].setEditable(false);
+            }
+        },
+        ///CRUD METHODS
+        this.sendPUT = function (model, obj) {
+            model.update("/Fridges('" + obj.frid + "')", obj, {
+                merge: false,
+                success: function () {
+                    createSuccessOnUpdateDialog();
+                },
+                error: function () {
+                    jQuery.sap.log.error("Error at PUT request");
+                }
+            })
+        },
+        this.sendPOST = function (model, obj) {
+            model.create("/Fridges", obj, {
+                merge: false,
+                success: function () {
+                    createSuccessOnCreateDialog();
+                },
+                error: function () {
+                    jQuery.sap.log.error("Error at POST request");
+                }
+            })
+        },
+        this.sendDEL = function (model, mId, config) {
+            var settings = {
                 "async": true,
                 "crossDomain": true,
-                "url": "https://p2001081083trial-trial-dev-router.cfapps.eu10.hana.ondemand.com/api/xsjs/fridge/fridge.xsjs?frid=" + frid,
+                "url": config.mainTableModelDeleteRequestURL + mId,
                 "method": "DELETE",
                 "headers": {
                     "content-type": "application/json"
                 },
                 "processData": false
             };
-            $.ajax(settings).done(function (response) {
-                obj.selItem.getBindingContext("fridges").getModel().refresh(true);
+            $.ajax(settings).done(function () {
+                model.refresh(true);
             });
+        }
+    this.createFridge = function (bModel, mModel) {
+            let oModel = bModel.getData();
+            delete oModel.ts_create;
+            delete oModel.ts_update;
+            //creating json model
+            if (checkModelObject(oModel)) {
+                sendPOST(mModel, oModel);
+                return true;
+            }
+        },
+        this.updateFridge = function (items, index, config, bModel, mModel) {
+            let oModel = bModel.getData();
+            let indexRow = items[index].getCells();
+            //creating json model
+            oModel.frid = indexRow[config.fridgeIdInputPosition].getValue();
+            oModel.bname = indexRow[config.brandNameInputPosition].getValue();
+            oModel.cap = indexRow[config.capacityInputPosition].getValue();
+            oModel.ts_create = null;
+            oModel.ts_update = null;
+            if (checkModelObject(oModel)) {
+                sendPUT(mModel, oModel);
+                return true;
+            }
+        }
+    this.deleteFridge = function (items, index, config, mModel) {
+        let frid = items[index].getCells()[config.fridgeIdInputPosition].getValue();
+        sendDEL(mModel, frid, config);
+    }
+    return BaseController.extend("fridge_display.controller.App", {
+        onInit: function () {
+            this.oView = this.getView();
+            this.mConfig = this.oView.getModel("config").getData();
+            this.mFridge = this.oView.getModel("fridgeModel");
+            this.fridges = this.oView.getModel(this.mConfig.mainTableModelName);
+            this.mainTable = this.oView.byId(this.mConfig.mainTableId);
+        },
+        ///FRAGMENTS
+        showCreateDialog: function () {
+            let view = this.oView;
+            if (!view.byId("createDialog")) {
+                Fragment.load({
+                    id: view.getId(),
+                    name: "fridge_display.view.Create",
+                    controller: this
+                }).then(function (oDialog) {
+                    view.addDependent(oDialog);
+                    oDialog.open();
+                });
+            } else {
+                view.byId("createDialog").open();
+            }
+        },
+        closeDialog: function () {
+            this.oView.byId("createDialog").close();
+        },
+
+        /// CRUD BUTTONS
+        createButton: function () {
+            createFridge(this.mFridge, this.fridges);
+        },
+        editButton: function (oEvent) {
+            let index = this.getTableRowIndex(oEvent, this.mainTable, this.mConfig.mainTableModelName, this.mConfig.idPropName);
+            editMode(this.mainTable.getItems(), index, this.mConfig);
+        },
+
+        saveButton: function (oEvent) {
+            let index = getTableRowIndex(oEvent, this.mainTable, this.mConfig.mainTableModelName, this.mConfig.idPropName);
+            if (updateFridge(this.mainTable.getItems(), index, this.mConfig, this.mFridge, this.fridges)) {
+                editMode(this.mainTable.getItems(), index, this.mConfig);
+            }
+        },
+
+        deleteButton: function (oEvent) {
+            let index = getTableRowIndex(oEvent, this.mainTable, this.mConfig.mainTableModelName, this.mConfig.idPropName);
+            deleteFridge(this.mainTable.getItems(), index, this.mConfig, this.fridges)
         }
     });
 });
